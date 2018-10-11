38976d8acf2dbec6eb0b30337750f47532c2af7b
greg_ <greg_s_snider@outlook.com>
2018-10-11 06:49:43 -0700

    checkpoint

--- analyzer.py
+++ analyzer.py
@@ -11,30 +11,38 @@ from PIL import ImageTk, Image, ImageDraw
 import munsell
 
 
-# Create Window for color analysis
-window = Tk()
-window.configure(background='grey')
+if __name__ == '__main__':
+    # Create Window for color analysis
+    window = Tk()
+    window.configure(background='grey')
 
-# Get image file to display
-filename = askopenfilename(title='Select file')
-window.title(os.path.basename(filename))
+    # Get image file to display
+    filename = askopenfilename(title='Select file')
+    window.title(os.path.basename(filename))
 
-# Display image and color information of pixel pointed at by mouse
-raw_image = Image.open(filename)
-img = ImageTk.PhotoImage(raw_image)
-panel = Label(window, image = img)
-location = StringVar()
-location.set('hello there')
-position = Label(window, textvar=location)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
-position.pack(side = 'top', fill='both', expand='yes')
+    # Display image and color information of pixel pointed at by mouse
+    raw_image = Image.open(filename)
+    img = ImageTk.PhotoImage(raw_image)
+    panel = Label(window, image = img)
+    location = StringVar()
+    location.set('hello there')
+    position = Label(window, textvar=location)
+    panel.pack(side = "bottom", fill = "both", expand = "yes")
+    position.pack(side = 'top', fill='both', expand='yes')
 
-def motion(event):
-    x, y = event.x, event.y
-    rgb = raw_image.getpixel((x, y))
-    #color = munsell.from_rgb(rgb)
-    location.set(str((x, y)) + str(rgb))
+    def clamp(n, smallest, largest):
+        return max(smallest, min(n, largest))
 
-window.bind('<Motion>', motion)
-window.mainloop()
+    def motion(event):
+        # Get mouse position clamped to borders of displayed image.
+        width = img.width()
+        height = img.height()
+        x, y = clamp(event.x, 0, width - 1), clamp(event.y, 0, height - 1)
 
+        # Display color info.
+        rgb = raw_image.getpixel((x, y))
+        #color = munsell.from_rgb(rgb)
+        location.set(str((x, y)) + str(rgb))
+
+    window.bind('<Motion>', motion)
+    window.mainloop()
