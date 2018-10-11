183814dff39bbe2b3a2ab01eb65244520a666d01
greg_ <greg_s_snider@outlook.com>
2018-10-11 06:34:28 -0700

    checkpoint

--- analyzer.py
+++ analyzer.py
@@ -1,40 +1,38 @@
-import sys
-from tkinter import Label, Toplevel, Tk, StringVar
-from tkinter.filedialog import askopenfilename
+""" Analyze pixels in a color image.
 
+Prompts the user for an image file name then displays it. Shows the RGB
+and Munsell color for the pixel pointed at by the mouse.
+
+"""
+import os
+from tkinter import Label, Tk, StringVar
+from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image, ImageDraw
 import munsell
 
 
-
-# Display it.
-window = Tk()  #Toplevel()
-window.title('Color Analyzer')
+# Create Window for color analysis
+window = Tk()
 window.configure(background='grey')
 
 # Get image file to display
-print("Color Analyzer")
 filename = askopenfilename(title='Select file')
-print('file', filename)
+window.title(os.path.basename(filename))
 
-
-#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-print(filename)
+# Display image and color information of pixel pointed at by mouse
 raw_image = Image.open(filename)
 img = ImageTk.PhotoImage(raw_image)
-print(img)
 panel = Label(window, image = img)
 location = StringVar()
 location.set('hello there')
 position = Label(window, textvar=location)
-#The Pack geometry manager packs widgets in rows or columns.
 panel.pack(side = "bottom", fill = "both", expand = "yes")
 position.pack(side = 'top', fill='both', expand='yes')
+
 def motion(event):
     x, y = event.x, event.y
     rgb = raw_image.getpixel((x, y))
     #color = munsell.from_rgb(rgb)
-    print(rgb)
     location.set(str((x, y)) + str(rgb))
 
 window.bind('<Motion>', motion)
