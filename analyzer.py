457eb53a0568f2bd247ba87aa3f0fbd9d238e67e
greg_ <greg_s_snider@outlook.com>
2018-10-11 05:57:32 -0700

    checkpoint

--- analyzer.py
+++ analyzer.py
@@ -1,18 +1,22 @@
-from tkinter import Label, Toplevel
+import sys
+from tkinter import Label, Toplevel, Tk, StringVar
 from tkinter.filedialog import askopenfilename
-from PIL import ImageTk, Image
+
+from PIL import ImageTk, Image, ImageDraw
 import munsell
 
 
-# Get image file to display
-print("Color Analyzer")
-filename = askopenfilename()
-print('file', filename)
 
 # Display it.
-window = Toplevel()
+window = Tk()  #Toplevel()
 window.title('Color Analyzer')
 window.configure(background='grey')
+
+# Get image file to display
+print("Color Analyzer")
+filename = askopenfilename(title='Select file')
+print('file', filename)
+
 
 #The Label widget is a standard Tkinter widget used to display a text or image on the screen.
 print(filename)
@@ -20,14 +24,18 @@ raw_image = Image.open(filename)
 img = ImageTk.PhotoImage(raw_image)
 print(img)
 panel = Label(window, image = img)
-
+location = StringVar()
+location.set('hello there')
+position = Label(window, textvar=location)
 #The Pack geometry manager packs widgets in rows or columns.
 panel.pack(side = "bottom", fill = "both", expand = "yes")
+position.pack(side = 'top', fill='both', expand='yes')
 def motion(event):
     x, y = event.x, event.y
     rgb = raw_image.getpixel((x, y))
-    color = munsell.from_rgb(rgb)
-    print(rgb, color)
+    #color = munsell.from_rgb(rgb)
+    print(rgb)
+    location.set(str((x, y)) + str(rgb))
 
 window.bind('<Motion>', motion)
 window.mainloop()
